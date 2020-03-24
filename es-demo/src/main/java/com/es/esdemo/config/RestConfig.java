302c97cb39f96e9a6b10a25d11fcd96c333031f8
zhouwen <644988358@qq.com>
2020-03-24 16:15:57 +0800

    初始化部分配置

---
+++ es-demo/src/main/java/com/es/esdemo/config/RestConfig.java
@@ -0,0 +1,26 @@
+package com.es.esdemo.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+@Configuration
+public class RestConfig {
+    @Bean
+    public RestClient getClient() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        // 如果有多个从节点可以持续在内部new多个HttpHost，参数1是ip,参数2是HTTP端口，参数3是通信协议
+        RestClientBuilder clientBuilder = RestClient.builder(new HttpHost("localhost", 9200, "http"));
+
+        // 添加其他配置，返回来的还是RestClientBuilder对象，这些配置都是可选的
+        // clientBuilder.setXX()...
+
+        // 最后配置好的clientBuilder再build一下即可得到真正的Client
+        return clientBuilder.build();
+    }
+}
